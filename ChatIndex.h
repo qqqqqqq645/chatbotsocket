4f49edbf4a8b88e7b4634a6f3ebd09913e1d7d1b
qqqqqq645 <dregdog1234@gmail.com>
2019-10-03 13:12:09 +0900

    헤더파일 분리

--- ChatIndex.h
+++ ChatIndex.h
@@ -4,20 +4,5 @@
 #define SERVERMESSAGE "Hi, I'm server\n"
 #define __BUFFSIZE 100
 
-char * delCmdAndSpace(char * dest,char * cmmd){
-	int cnt = 0;
-	//명령어와 문자열에있는 문자열 비교
-	while(*dest!='\0' && *cmmd!='\0' && *dest==*cmmd){
-		cnt++;
-		dest++;
-		cmmd++;
-	} //명령어와 문자열사이의 공백 제거
-	while(*dest== ' ' || *cmmd == ' '){
-		cnt++;
-		dest++;
-		cmmd++;
-	}
-
-	return dest;
-}
+char * delCmdAndSpace(char * dest,char * cmmd);
 #endif
